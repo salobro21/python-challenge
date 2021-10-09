ed351c5bf1e1b9bcabc4ae1e0fc5b5ac10d327d9
salobro <90297288+salobro21@users.noreply.github.com>
2021-10-09 16:55:56 -0500

    some quick fixes
    comments were needed on the code

--- PyBank/main.py
+++ PyBank/main.py
@@ -10,38 +10,49 @@ change = 0
 dates = []
 profits = []
 
+# open and read the csv data
 with open(budget_data, newline = "") as csvfile:
     csvreader = csv.reader(csvfile, delimiter = ",")
 
+    # reading the headers
     csv_header = next(csvreader)
 
+    # first row 
     first_row = next(csvreader)
     total_months += 1
     total_pl += int(first_row[1])
     value = int(first_row[1])
     
+    # the rest of the data 
     for row in csvreader:
+        # date list
         dates.append(row[0])
         
+        # change calculation
         change = int(row[1])-value
         profits.append(change)
         value = int(row[1])
-        
-        total_months += 1
 
+        # tracking the months
+        total_months += 1
+    
+        # the profit/loss over time
         total_pl = total_pl + int(row[1])
 
+    # greatest increase
     greatest_increase = max(profits)
     greatest_index = profits.index(greatest_increase)
     greatest_date = dates[greatest_index]
 
+    # greatest decrease
     greatest_decrease = min(profits)
     worst_index = profits.index(greatest_decrease)
     worst_date = dates[worst_index]
 
+    # average change of profits/losses
     avg_change = sum(profits)/len(profits)
     
-
+# display the info
 print("Financial Analysis")
 print("---------------------")
 print(f"Total Months: {str(total_months)}")
@@ -50,6 +61,7 @@ print(f"Average Change: ${str(round(avg_change,2))}")
 print(f"Greatest Increase in Profits: {greatest_date} (${str(greatest_increase)})")
 print(f"Greatest Decrease in Profits: {worst_date} (${str(greatest_decrease)})")
 
+# print the info to a text doc
 output = open("output.txt", "w")
 
 line1 = "Financial Analysis"
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -3,12 +3,16 @@ import csv
 
 election_data = os.path.join("D://HW3//python-challenge//PyPoll//Resources//election_data.csv")
 
+# candidate list
 candidates = []
 
+# votes per candidate
 num_votes = []
 
+# percentage total votes per person
 percent_votes = []
 
+# total votes 
 total_votes = 0
 
 with open(election_data, newline = "") as csvfile:
@@ -16,8 +20,8 @@ with open(election_data, newline = "") as csvfile:
     csv_header = next(csvreader)
 
     for row in csvreader:
+        # add votes
         total_votes += 1 
-
     
         if row[2] not in candidates:
             candidates.append(row[2])
@@ -27,18 +31,19 @@ with open(election_data, newline = "") as csvfile:
             index = candidates.index(row[2])
             num_votes[index] += 1
     
-    
+    # tracking percentages
     for votes in num_votes:
         percentage = (votes/total_votes) * 100
         percentage = round(percentage)
         percentage = "%.3f%%" % percentage
         percent_votes.append(percentage)
     
-
+# find the winner
     winner = max(num_votes)
     index = num_votes.index(winner)
     winning_candidate = candidates[index]
 
+#  display the results
 print("Election Results")
 print("--------------------------")
 print(f"Total Votes: {str(total_votes)}")
@@ -49,6 +54,7 @@ print("--------------------------")
 print(f"Winner: {winning_candidate}")
 print("--------------------------")
 
+# print to a txt doc
 output = open("output.txt", "w")
 line1 = "Election Results"
 line2 = "--------------------------"
